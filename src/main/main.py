fcb1fdf48c20d8b3c129b098e15aa0c908030d32
Thanpisit Banyam <porsohani@gmail.com>
2026-04-01 01:40:12 +0700

    feat: create main.py for robot simulation initialization

---
+++ src/main/main.py
@@ -0,0 +1,9 @@
+from coppeliasim_zmqremoteapi_client import RemoteAPIClient
+import numpy as np
+from src.Inverse Kinematic.main
+
+# --- Initialization ---
+client = RemoteAPIClient()
+sim = client.require('sim')
+sim.setStepping(True) 
+
